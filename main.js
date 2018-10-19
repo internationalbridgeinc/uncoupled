d7197edd7dc7f32ea56b0f506d33c2c324f59afd
Milos Mrdovic <milosh.mrdovich@myib.com>
2018-10-19 12:44:11 -0700

    Add application initialization method.

--- main.js
+++ main.js
@@ -4,8 +4,12 @@ const Route = require('./src/route');
 const Controller = require('./src/api-controller');
 const Container = require('./src/di-container');
 
-module.exports = function () {
-    return Container;
+module.exports = function (app, settings) {
+    const container = new Container(settings.dependencies);
+    const route = new Route(app, container);
+    for (let line of settings.routes) {
+        route[line[0]].apply(route, line.splice(1));
+    }
 };
 module.exports.route = Route;
 module.exports.controller = Controller;
